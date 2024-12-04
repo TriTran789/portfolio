078da050984c327e7c0c585eb4be0e6c89b8ed55
ThanhTris <thanhtritran8@gmail.com>
2024-12-04 15:38:12 +0700

    update infor

--- components/Infor.tsx
+++ components/Infor.tsx
@@ -4,13 +4,17 @@ const Infor = () => {
       id="infor"
       className="bg-[#08110c] px-4 py-20 sm:px-6 md:px-8 lg:px-20 text-white"
     >
-      <div className="bg-[#14213d] rounded-2xl w-full border border-white/10 px-4 py-8 flex flex-col lg:flex-row">
-        <div>
+      <h1 className="mb-8 text-3xl font-bold tracking-wider">INFOR</h1>
+      <div className="bg-[#14213d] rounded-2xl w-full border border-white/10 px-6 md:px-16 py-8 flex flex-col lg:flex-row gap-8 md:gap-0">
+        <div className="flex-1">
           <h1>TRAN THANH TRI</h1>
           <p>Birthday: 03/07/2003</p>
+          <p>Gender: Male</p>
         </div>
-        <div>
-          
+        <div className="flex-1">
+          <h1>ACADEMY OF CRYPTOGRAPHY TECHNIQUES</h1>
+          <p>Department: Software Engineer</p>
+          <p>School year: 2021 - 2025</p>
         </div>
       </div>
     </div>
