4a08decae2ec735612968a8dc726d9806b232cb2
ThanhTris <thanhtritran8@gmail.com>
2024-12-04 15:13:30 +0700

    add name

--- components/Infor.tsx
+++ components/Infor.tsx
@@ -1,7 +1,20 @@
 const Infor = () => {
   return (
-    <div id="infor" className="bg-[#08110c] text-white">Infor</div>
-  )
-}
+    <div
+      id="infor"
+      className="bg-[#08110c] px-4 py-20 sm:px-6 md:px-8 lg:px-20 text-white"
+    >
+      <div className="bg-[#14213d] rounded-2xl w-full border border-white/10 px-4 py-8 flex flex-col lg:flex-row">
+        <div>
+          <h1>TRAN THANH TRI</h1>
+          <p>Birthday: 03/07/2003</p>
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Infor
+export default Infor;
